45692d441a3ad92f2a65805db6108213940ba74c
Mariano Pardo <marianomdq1@gmail.com>
2014-03-13 12:38:25 -0300

    Moved translations to a separate file

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -28,46 +28,4 @@ angular.module('labApp', [
       .otherwise({
         redirectTo: '/'
       });
-  })
-  // Translations
-  .config(function ($translateProvider) {
-    $translateProvider.translations('en', {
-      home: 'Home',
-      login: 'Login with GitHub',
-      logout: 'Logout',
-      welcome: 'Welcome',
-      loading: 'Loading...',
-      more_info: 'More Info',
-      repository: 'Repository',
-      watchers: 'Watchers',
-      forks: 'Forks',
-      contributors: 'Contributors',
-      last_year_commits: 'Last year of commit activity per week',
-      number_of_additions_deletions: 'Number of additions and deletions per week',
-      languages: 'Languages',
-      you_have: 'You have',
-      repositories_starred: 'repositories starred',
-      unstar: 'Unstar',
-      login_github_message: 'Login with GitHub to see your starred repos!'
-    });
-    $translateProvider.translations('es', {
-      home: 'Inicio',
-      login: 'Entrar con GitHub',
-      logout: 'Salir',
-      welcome: 'Bienvenido',
-      loading: 'Cargando...',
-      more_info: 'Mas informacion',
-      repository: 'Repositiorios',
-      watchers: 'Observadores',
-      forks: 'Forks',
-      contributors: 'Contribuidores',
-      last_year_commits: 'Actividad de commits por semana del ultimo año',
-      number_of_additions_deletions: 'Numero de lineas agregadas y eliminadas por semana',
-      languages: 'Idiomas',
-      you_have: 'Usted tiene',
-      repositories_starred: 'repositorios en favoritos',
-      unstar: 'Eliminar',
-      login_github_message: 'Entra con GitHub para ver tus repositorios en favoritos'
-    });
-    $translateProvider.preferredLanguage('en');
   });
